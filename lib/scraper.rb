d17828ad7cb7c553af77b37ac2bbd9a3cf86f891
Tao <werewaswas@live.com>
2018-07-28 00:34:09 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ require_relative './course.rb'
 class Scraper
   def self.get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-      binding.pry
+    binding.pry
     #expect(doc).to be_a(Nokogiri::HTML::Document)
   end
 
